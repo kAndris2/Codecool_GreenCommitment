300a4e73dfd79fa981624e1af951b3cf11ccf4b8
andri <andri@DESKTOP-FIORF47>
2020-03-03 09:06:04 +0100

    update ClientImpl class

--- GreenCom/Client/ClientImpl.cs
+++ GreenCom/Client/ClientImpl.cs
@@ -8,71 +8,45 @@ namespace Client
 {
     public class ClientImpl
     {
-        static void Main(string[] args)
+        TcpClient Client;
+        NetworkStream Ns;
+        Random Random;
+
+        public ClientImpl(string ip, int port)
         {
+            //Client = new TcpClient(GetLocalIPAddress(), 12345);
+            Client = new TcpClient(ip, port);
+            Ns = Client.GetStream();
+            Random = new Random();
+        }
 
-            string GetLocalIPAddress()
+        public void SendMeasurement(string name)
+        {
+            Measurement measure = new Measurement(Random.Next(1000, 9999),
+                                                  Random.Next(-200, 200),
+                                                  name,
+                                                  DateTimeOffset.Now.ToUnixTimeMilliseconds());
+
+            byte[] msg = Encoding.ASCII.GetBytes(measure.ToString());
+            Ns.Write(msg);
+        }
+
+        public void CloseClient()
+        {
+            Client.Close();
+        }
+
+        private string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
             {
-                var host = Dns.GetHostEntry(Dns.GetHostName());
-                foreach (var ip in host.AddressList)
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
                 {
-                    if (ip.AddressFamily == AddressFamily.InterNetwork)
-                    {
-                        return ip.ToString();
-                    }
+                    return ip.ToString();
                 }
-                throw new Exception("No network adapters with an IPv4 address in the system!");
             }
-
-
-            TcpClient client = new TcpClient(GetLocalIPAddress(), 12345);
-
-            NetworkStream ns = client.GetStream();
-
-            Random random = new Random();
-            Console.WriteLine("Enter your value type:\n");
-            Measurement measure = new Measurement(random.Next(1000, 9999),
-                                                  random.Next(-150, 150),
-                                                  Console.ReadLine(),
-                                                  DateTimeOffset.Now.ToUnixTimeMilliseconds());
-
-            byte[] msg = Encoding.ASCII.GetBytes(measure.ToString());
-            // int bytesRead = await ns.ReadAsync(bytes, 0, bytes.Length);
-            //string result = Encoding.ASCII.GetString(bytes, 0, bytesRead);
-            ns.Write(msg);
-
-            client.Close();
-            /*
-            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
-            IPAddress ipAddress = ipHostInfo.AddressList[0];
-
-            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 12345);
-
-            // Create a TCP/IP  socket.  
-            Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-
-            // Connect the socket to the remote endpoint. Catch any errors.  
-            sender.Connect(remoteEP);
-
-            Console.WriteLine("Socket connected to {0}", sender.RemoteEndPoint.ToString());
-
-            Random random = new Random();
-            Console.WriteLine("Enter your value type:\n");
-            Measurement measure = new Measurement(random.Next(1000, 9999),
-                                                  random.Next(-150, 150),
-                                                  Console.ReadLine(),
-                                                  DateTimeOffset.Now.ToUnixTimeMilliseconds());
-
-            // Encode the data string into a byte array.  
-            byte[] msg = Encoding.ASCII.GetBytes(measure.ToString());
-
-            // Send the data through the socket.  
-            int bytesSent = sender.Send(msg);
-            Console.WriteLine("Sent shit: {0}", bytesSent);
-
-            sender.Shutdown(SocketShutdown.Both);
-            sender.Close();
-            */
+            throw new Exception("No network adapters with an IPv4 address in the system!");
         }
     }
 }
