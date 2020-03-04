395d3b1ede038c840ad57a7377a91fce963a3f25
viktorferenczi <ferencziviktor11@gmail.com>
2020-03-04 13:10:40 +0100

    data save

--- GreenCom/Common/Measurement.cs
+++ GreenCom/Common/Measurement.cs
@@ -7,9 +7,9 @@ namespace Common
         public int Id { get; set; }
         public int Value { get; set; }
         public String Type { get; set; }
-        public long Time { get; set; }
+        public double Time { get; set; }
 
-        public Measurement(int id, int value, string type, long time)
+        public Measurement(int id, int value, string type, double time)
         {
             Id = id;
             Value = value;
@@ -22,8 +22,14 @@ namespace Common
             Id = Convert.ToInt32(str[0]);
             Value = Convert.ToInt32(str[1]);
             Type = str[2];
+            string iString = str[3];
+            DateTime oDate = DateTime.ParseExact(iString, "yyyy-MM-dd HH:mm tt", null);
             //Time = Convert.ToInt64(str[3]);
-            Time = new DateTimeOffset(str[3]).ToUnixTimeMilliseconds();
+            Time = oDate.ToUniversalTime().Subtract(
+    new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)
+    ).TotalMilliseconds;
+
+
         }
 
         public Measurement()
